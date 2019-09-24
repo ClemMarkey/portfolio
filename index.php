875921b82ca15c71ee5f2466151575f9b1028dd2
ClemMarkey <markey.clement@gmail.com>
2019-09-24 17:05:51 +0200

    début

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Londrina+Outline&display=swap" rel="stylesheet">
+    <title>Clément Markey</title>
+</head>
+<body>
+
+<h1>Hey bonjour tout le monde lol</h1>
+
+<div class="coucou">
+    <p>
+        Salut !
+    </p>
+</div>
+
+</body>
+</html>
