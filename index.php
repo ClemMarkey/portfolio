fd5f2605b7c0fa125eed8466f04a7d5ab52eab38
ClemMarkey <markey.clement@gmail.com>
2019-09-25 13:53:35 +0200

    ajout variable scss + bootstrap

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.min.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
     <link href="https://fonts.googleapis.com/css?family=Lato|Londrina+Outline&display=swap" rel="stylesheet">
     <title>Clément Markey</title>
 </head>
+
 <body>
 
-<h1>Hey bonjour tout le monde lol</h1>
+    <h1>Hey bonjour tout le monde lol</h1>
 
-<div class="coucou">
-    <p>
-        Salut !
-    </p>
-</div>
+    <div class="coucou container">
+        <p>
+            Salut !
+        </p>
+    </div>
+
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
 
 </body>
+
 </html>
