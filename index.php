ae764abba570b4af5defe54da6ecccab69b4bf28
ClemMarkey <markey.clement@gmail.com>
2019-10-16 13:46:47 +0200

    refonte du style

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     
-    <link href="https://fonts.googleapis.com/css?family=Lato|Coming+Soon|Londrina+Outline&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.min.css">
     <title>Clément Markey</title>
 </head>
 
 <body>
 
-    <h1 class="flicker">Hey bonjour tout le monde</h1>
+    <h1><span class="glow">C</span>lément <span class="glow">M</span>arkey</h1>
 
     <div class="coucou border">
         <p>
-            Bonjour ! Je m'appelle Clément. Je suis développeur web dans la région de Lille et je recherche un CDI en développement Back-End Laravel.
+            Hello ! Je m'appelle Clément. Je suis développeur web dans la région de Lille et je recherche un CDI en développement Back-End Laravel.
         </p>
     </div>
 
