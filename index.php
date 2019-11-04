a25f98641abf5b38eb3204c31de29227c18bbace
ClemMarkey <markey.clement@gmail.com>
2019-11-04 11:42:19 +0100

    next step

--- index.php
+++ index.php
@@ -7,25 +7,57 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     
-    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Istok+Web|Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.min.css">
     <title>Clément Markey</title>
 </head>
 
 <body>
 
-    <h1><span class="glow">C</span>lément <span class="glow">M</span>arkey</h1>
+    <main>
 
-    <div class="coucou border">
-        <p>
-            Hello ! Je m'appelle Clément. Je suis développeur web dans la région de Lille et je recherche un CDI en développement Back-End Laravel.
-        </p>
+        <section class="top container">
+            <h1>Bonjour<br>à tous</h1>
+
+            <p class="container">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est voluptas neque necessitatibus ipsam numquam dolores magnam temporibus aliquid. Ipsum ad quo architecto assumenda, harum aperiam blanditiis doloremque consectetur cupiditate inventore. Ad odio tempora expedita minus inventore sed quas nostrum, beatae sunt nesciunt consectetur iusto nobis dolorum, reprehenderit soluta obcaecati tenetur. Sed cum illum animi, suscipit minima doloribus nobis eaque quibusdam iure obcaecati?
+            </p>
+        </section>
+
+        <section class="folio"></section>
+
+        <section class="contact"></section>
+
+        <section class="about"></section>
+
+    </main>
+
+    <footer class="footer">
+    <div class="container">
+        <div class="row nomargin">
+            <div class="col col--xs-12 col--sm-6 col--md-4 nopadding">
+                <p class="footer__text">© 2019 Clément Markey All rights reserved</p>
+            </div>
+
+            <div class="col col--xs-5 col--sm-6 col--md-1 nopadding">
+                <div class="mail">
+                    <a class="mail-link" href="mailto:clement.markey.pro@gmail.com">clement.markey.pro@gmail.com</a>
+                </div>
+            </div>
+
+            <div class="col col--xs-7 col--sm-6 col--md-3 nopadding">
+                <div class="tel">
+                    <a class="tel-link" href="tel:+33645613055">+33 6 45 61 30 55</a>
+                </div>
+            </div>
+        </div>
     </div>
+</footer>
 
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-            
 
 </body>
 
